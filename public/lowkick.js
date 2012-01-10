a684230c656b0f9edff1e7e8a255523604989edf
E.Azer Koçulu <azer@kodfabrik.com>
2012-01-09 20:57:56 -0800

    refresh results after sending ok/fail reqs

--- public/lowkick.js
+++ public/lowkick.js
@@ -15,11 +15,13 @@
   exports.ok = function ok(){
     lowkick.message('Setting test result of current environment as OK');
     lowkick.api.ok(environPropertyNames(), exports.results);
+    lowkick.layout.results();
   };
 
   exports.fail = function fail(){
     lowkick.message('Setting test result of current environment as FAIL');
     lowkick.api.fail(environPropertyNames(), exports.results);
+    lowkick.layout.results();
   };
 
 })(this.lowkick = {});
